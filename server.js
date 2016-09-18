2bb6f852072b7ca8d66db0f36187e252fb6e9181
shash152397 <shashwatraghuvanshi2014@gmail.com>
2016-09-18 09:15:16 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,6 +8,9 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+app.get('/article1', function (req, res) {
+  res.send("articleone is served");
+});
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
